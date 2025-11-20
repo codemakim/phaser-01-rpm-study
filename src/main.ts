9e2f2afa2e2edde391df9de821b0d5d1c580c115
codemakim <codemakim@gmail.com>
2025-11-20 12:58:10 +0900

    텍스트 키보드로 움직이기 구현, 플레이어 에셋 추가

--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,8 @@
 import Phaser from "phaser";
 
 class MainScene extends Phaser.Scene {
+  cursorKeys!: Phaser.Types.Input.Keyboard.CursorKeys;
+  text!: Phaser.GameObjects.Text;
   constructor() {
     super('MainSene');
   }
@@ -8,13 +10,30 @@ class MainScene extends Phaser.Scene {
     // 이미지, 스프라이트 로딩을 여기서 한다고 한다.
   }
   create() {
-    this.add.text(100, 100, 'Hellow Phaser!', {
+    this.text = this.add.text(100, 100, 'Hellow Phaser!', {
       fontSize: '32px'
     })
+    this.cursorKeys = this.input.keyboard?.createCursorKeys() as Phaser.Types.Input.Keyboard.CursorKeys;
+
   }
+  // time: 게임 시작 후 누적된 시간(ms)
+  // delta: 직전 프레임과의 시간 차(ms) - 60fps면 대량 16.6
   update(time: number, delta: number) {
     // 매 프레임마다 호출되는 함수 (게임 루프)
-    console.log('time: ', time, 'delta: ', delta)
+    const speed = 200;
+    const dt = delta / 1000;
+    if (this.cursorKeys.left?.isDown) {
+      this.text.x -= speed * dt;
+    }
+    if (this.cursorKeys.right?.isDown) {
+      this.text.x += speed * dt;
+    }
+    if (this.cursorKeys.up?.isDown) {
+      this.text.y -= speed * dt;
+    }
+    if (this.cursorKeys.down?.isDown) {
+      this.text.y += speed * dt;
+    }
   }
 }
 
